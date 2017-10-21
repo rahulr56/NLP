8a37a83137f4c63bbb6c624914430fd649c076fc
rahulr56 <rahulr301@gmail.com>
2017-10-21 11:39:09 -0400

    training data read and dict is prepared

--- Naive_Bayes_Text_Classification/src/textClassifier.py
+++ Naive_Bayes_Text_Classification/src/textClassifier.py
@@ -1,4 +1,7 @@
+import re
+import pprint
 
+pp = pprint.PrettyPrinter(indent=3)
 
 def readfile(fileName):
     data = ""
@@ -12,16 +15,50 @@ def readfile(fileName):
     return data
 
 
+def __getSpeaketDict(speakerData):
+    vocabSize = ""
+    for speaker in speakerData:
+        data = speakerData[speaker]['statement'].strip()
+        setWords = set(data.split(' '))
+        for word in setWords:
+            wc = re.findall(word, data)
+            speakerData[speaker]["words"][word] = len(wc)
+        vocabSize += data+" "
+    return speakerData, len(set(vocabSize.strip().split(' ')))
+
+
+def parseSpeakerData(data):
+    speakers = {}
+    totalwords = 0
+    for line in data:
+        line = re.sub('[ ]+',' ', re.sub('[.,\?]',' ',line.strip()))
+        speakerName = line.split(' ')[0]
+        ldata = line.split(' ')[1:]
+        totalwords += len(ldata)
+
+        if speakers.get(speakerName):
+            speakers[speakerName]["statement"] += " ".join(ldata)+" "
+            speakers[speakerName]["docCount"] += 1
+            speakers[speakerName]["wordCount"] += len(ldata)
+        else:
+            speakers[speakerName] = {"statement":"", "docCount":1, "wordCount":len(ldata), "words":{}}
+            speakers[speakerName]["statement"]= " ".join(ldata)+" "
+
+    speakers, vocabSize = __getSpeaketDict(speakers)
+    print("vocabSize :"+str(vocabSize))
+    return speakers, totalwords, vocabSize
+
 
 def main():
     data = readfile("../data/train")
-    speakers = []
-    for line in data:
-        line = line.strip()
-        speakerName = line.split(' ')[0]
-        speakers.append(speakerName)
-    for i in set(speakers):
-        print (i)
+    speakerData, totalwords, vocabSize= parseSpeakerData(data)
+
+    pp.pprint(speakerData)
+    print ("Main total words :  ")
+    print(totalwords)
+    print ("Main Vocab words :  ")
+    print(vocabSize)
+
 
 
 if __name__=="__main__":
