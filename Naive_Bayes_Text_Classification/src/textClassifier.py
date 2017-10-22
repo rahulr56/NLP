9d717903512c2eddd6b201131d0286a3662a7806
rahulr56 <rahulr301@gmail.com>
2017-10-21 23:58:01 -0400

    refactored code

--- Naive_Bayes_Text_Classification/src/textClassifier.py
+++ Naive_Bayes_Text_Classification/src/textClassifier.py
@@ -1,6 +1,8 @@
 import re
 import math
 import pprint
+from nltk import FreqDist as fd
+from nltk import word_tokenize
 
 pp = pprint.PrettyPrinter(indent=3)
 
@@ -11,6 +13,7 @@ class NaiveBayes:
     speakerData = {}
     totalTrainData = ""
     bagOfWords = {}
+    totalWords = 0
 
     def __readfile(self, fileName):
         data = ""
@@ -24,58 +27,42 @@ class NaiveBayes:
         return data
 
 
-    def __genClassVocab(self):
-        for speaker in self.speakerData.keys():
-            data = self.speakerData[speaker].get("statement").strip()
-            lVocabSize = len(set(data.split(' ')))
-            self.speakerData[speaker]["classVocabSize"] = lVocabSize
-
-    def __getSpeaketDict(self, speakerData):
-        vocabSize = ""
-        for speaker in speakerData:
-            data = speakerData[speaker]['statement'].strip()
+    def __getSpeaketDict(self):
+        for speaker in self.speakerData:
+            data = self.speakerData[speaker]['statement'].strip()
             self.totalTrainData += data+" "
-            setWords = set(data.split(' '))
-            for word in setWords:
-                wc = re.findall(word, data)
-                speakerData[speaker]["words"][word] = len(wc)
-            vocabSize += data+" "
-        return speakerData, len(set(vocabSize.strip().split(' ')))
+            self.speakerData[speaker]["words"] = dict(fd(word_tokenize(data)))
+            self.speakerData[speaker]["classVocabSize"] = len(self.speakerData[speaker]["words"].keys())
 
 
     def __genBagOfWords(self):
-        data = set(self.totalTrainData.strip().split(' '))
-        for word in data:
-            self.bagOfWords[word] = len(re.findall(word, self.totalTrainData))
+        self.bagOfWords = dict(fd(word_tokenize(self.totalTrainData.strip())))
+        self.vocabSize = len(self.bagOfWords.keys())
+        self.totalwords = sum(self.bagOfWords.values())
 
 
     def __prepareData(self, data):
-        data = re.sub('[ ]+',' ', re.sub('[.,\?]',' ',data.strip()))
+        data = re.sub('[ ]+',' ', re.sub('[.,\?!-]',' ',data.strip()))
+        data = re.sub("[']","",data)
         data = data.strip().split(' ')
         return data[0],data[1:]
 
 
     def parseTrainingData(self, fileName):
-        speakers = {}
         data = self.__readfile(fileName)
         self.totalDocs = len(data)
-        self.totalwords = 0
         for line in data:
-            # line = re.sub('[ ]+',' ', re.sub('[.,\?]',' ',line.strip()))
             speakerName, ldata = self.__prepareData(line)
-            self.totalwords += len(ldata)
-
-            if speakers.get(speakerName):
-                speakers[speakerName]["statement"] += " ".join(ldata)+" "
-                speakers[speakerName]["docCount"] += 1
-                speakers[speakerName]["wordCount"] += len(ldata)
+            if self.speakerData.get(speakerName):
+                self.speakerData[speakerName]["statement"] += " ".join(ldata)+" "
+                self.speakerData[speakerName]["docCount"] += 1
+                self.speakerData[speakerName]["wordCount"] += len(ldata)
             else:
-                speakers[speakerName] = {"statement":"", "docCount":1, "wordCount":len(ldata), "words":{}}
-                speakers[speakerName]["statement"]= " ".join(ldata)+" "
+                self.speakerData[speakerName] = {"statement":"", "docCount":1, "wordCount":len(ldata), "words":{}}
+                self.speakerData[speakerName]["statement"]= " ".join(ldata)+" "
 
-        self.speakerData, self.vocabSize = self.__getSpeaketDict(speakers)
+        self.__getSpeaketDict()
         self.__genBagOfWords()
-        self.__genClassVocab()
 
 
     def parseTestData(self, fileName):
@@ -84,9 +71,9 @@ class NaiveBayes:
         for line in testData:
             speakerName, speakerStatement = self.__prepareData(line)
             if testSpeakersData.get(speakerName):
-                testSpeakersData[speakerName].append(" ".join(speakerStatement))
+                testSpeakersData[speakerName].append(" ".join(speakerStatement).strip())
             else:
-                testSpeakersData[speakerName] = [" ".join(speakerStatement)]
+                testSpeakersData[speakerName] = [" ".join(speakerStatement).strip()]
         return testSpeakersData
 
 
@@ -97,9 +84,8 @@ class NaiveBayes:
             localScore = 0
             classProb = float(self.speakerData[speaker].get("docCount")/float(self.totalDocs))
             for i in range(len(data)):
-                count = self.speakerData[speaker]["words"].get(data[i]) or 1
+                count = self.speakerData[speaker]["words"].get(data[i], 0)
                 localScore += math.log((count + 1.0)/(self.vocabSize + self.speakerData[speaker].get("wordCount")))
-                # localScore += math.log(count/float(self.speakerData[speaker].get("wordCount")))
             result[localScore + math.log(classProb)] = speaker
         predictedSpeaker = result[max(result.keys())]
         return predictedSpeaker
@@ -111,7 +97,7 @@ class NaiveBayes:
             dataToPredict = testData[actualSpeaker]
             for statement in dataToPredict:
                 predictedSpeaker = self.score(statement)
-                print (actualSpeaker+"\t:\t"+predictedSpeaker)
+        #        print (actualSpeaker+"\t:\t"+predictedSpeaker)
                 if actualSpeaker == predictedSpeaker:
                     positiveCount += 1
                 else:
@@ -125,17 +111,22 @@ class NaiveBayes:
         print ("Total Number of Words :  "+str(self.totalwords))
         print ("Vocab Size :  "+str(self.vocabSize) )
         print ("Total number of Docs : "+str(self.totalDocs))
-        print ("Bag of words for the whole training data : " )
-        pp.pprint(self.bagOfWords)
+        # print ("Bag of words for the whole training data : " )
+        # print(self.bagOfWords)
         print ("Speakers present in the training data : ")
         pp.pprint(self.speakerData.keys())
         print ("")
 
 
 def main():
+    print ("# Intializing the model")
     nb = NaiveBayes()
+    print ("# Building the model using training data ")
     nb.parseTrainingData("../data/train")
+    nb.printTrainingModel()
+    print ("# Parsing the test dataset")
     testData = nb.parseTestData("../data/test")
+    print ("# Testing the model using test data ")
     nb.computeModel(testData)
 
 
