53f8fd66ee6b10a43da3419d83b21fec10d9f787
rahulr566 <rahulr301@gmail.com>
2017-11-30 21:20:16 -0500

    Adding basic files

---
+++ MachineTranslation/src/IBMTranslate.py
@@ -0,0 +1,12 @@
+#!/bin/python
+import nltk
+
+from nltk import word_tokenize
+from nltk import FreqDist as fd
+from nltk.corpus import stopwords
+from nltk.collocations import BigramCollocationFinder
+
+class IBMModel:
+    def __init__():
+        self.bigrams={}
+
